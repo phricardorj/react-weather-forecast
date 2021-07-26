b4e3ac35d2f1b0b875095bc56114f57d76257fa7
Pedro Henrique Ricardo <70300680+phricardorj@users.noreply.github.com>
2021-07-26 05:01:38 -0300

    🎉 Primeiro Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# weatherforecast
-🌡️ | Find the weather of any city with this application. Based on the transmission of Satander Coders.
---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { api } from './services/api'
+import { FaTemperatureHigh, FaWind } from 'react-icons/fa'
+
+function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('');
+  const [search, setSearch] = useState('');
+
+
+  async function handleGetWeather(event) {
+    event.preventDefault();
+
+    const response = await api.get(search)
+    console.log(response)
+    setCity(search)
+    setWeather(response.data)
+  }
+  useEffect(() => {
+    handleGetWeather()
+  }, [search])
+
+  return (
+    <div className="App">
+      <header>
+        <p id="info">Pesquise pelo nome da cidade</p>
+        <form onSubmit={handleGetWeather}>
+          <input type="text" value={search} onChange={(event) => setSearch(event.target.value)} />
+          <button >Search</button>
+        </form>
+      </header>
+
+      {weather &&
+        <main>
+
+          <h1>{city}</h1>
+
+          <section className='current-weather'>
+            <h2>Current weather</h2>
+
+            <p>{weather.temperature}</p>
+            <p>{weather.description}</p>
+
+          </section>
+
+          <section className='forecast'>
+            <h2>Forecast</h2>
+            <ol>
+              {
+                weather.forecast.map(day =>
+                  <li>
+                    <div>
+                      <FaTemperatureHigh />
+                      <p>{day.temperature}</p>
+                    </div>
+
+                    <div>
+                      <FaWind />
+                      <p>{day.wind}</p>
+                    </div>
+                  </li>
+                )
+              }
+            </ol>
+          </section>
+        </main>
+      }
+    </div>
+  )
+}
+
+export default App
